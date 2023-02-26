b4da820bfda2501cf52ed8b971d99b42a049ae23
rishyak <hello@rishyak.com>
2023-02-25 19:56:33 -0500

    update: new AHB interface (#4)

--- rtl/ahb/interfaces/AHBCommon_if.sv
+++ rtl/ahb/interfaces/AHBCommon_if.sv
@@ -1,87 +1,119 @@
 /**
-  @brief Common Signals for a single-controller AMBA 5 AHB Interface
-
-  @note See https://www.arm.com/architecture/system-architectures/amba/amba-5
-
-  @param DataWidth  bit-width of data transfers
-  @param AddrWidth  bit-width of an address
-  @param PrphNum    number of peripherals supported by the interface
-
-  @input clk    clock
-  @input nReset active-low reset
-
-  @logic addr     byte address of the transfer
-  @logic burst    number of transfers in the burst
-  @logic mastLock if current transfer is part of a locked sequence
-  @logic size     size of the transfer
-  @logic nonSec   if transfer is non-secure
-  @logic trans    transfer type
-  @logic wData    write data from Ctrl -> Prph
-  @logic wStrb    write strobe
-  @logic write    transfer direction, high/write low/read
-
-  @logic rData    read data from Mux -> Ctrl
-  @logic ready    if previous transfer in complete
-  @logic resp     status of transfer
-*/
+  @brief Common signals for a single-manager AMBA 5 APB Interface
+  
+  @note See https://developer.arm.com/documentation/ihi0011/a/AMBA-AHB
+        and Ch 1 and 2 of AMBA Protocol Spec.
+  
+  @param DataWidth    bit-width of data transfers
+  @param AddrWidth    bit-width of addresses
+  @param protWidth    bit-width of protection signal controller
+  
+  @input clk      clock
+  @input nReset   active-low-reset
+  
+  @logic addr       byte address of the transfer
+  @logic burst      number of transfers in the burst
+  @logic mastLock   if current transfer is part of a locked sequence
+  @logic prot       protection control signal (access type information)
+  @logic size       size of the transfer
+  @logic nonSec     if the transfer is non-secure
+  @logic excl       if the transfer is part of an exvlusive access sequence
+  @logic trans      transfer type
+  @logic wData      write data from manager to subordinates
+  @logic wStrb      write strobe
+  @logic write      transfer direction, high/write low/read
+  @logic ready      indicates completion of previous transfer
+  
+  @logic rData      read data from subordinates to mux
+  @logic readyOut   indicates completion of transfer
+  @logic resp       transfer status, high/error low/error
+  @logic exOkay     status of exclusive transfer
+ */
 interface AHBCommon_if #(
-    DataWidth = 32,
-    AddrWidth = 32
+  DataWidth = 32,
+  AddrWidth = 32,
+  protWidth = 4
 ) (
-    input clk,
-    input nReset
+  input clk,
+  input nReset
 );
+  // Manager signals
   logic [AddrWidth - 1:0] addr;
   logic [2:0] burst;
   logic mastLock;
-  logic [2:0] size;
+  logic [protWidth - 1:0] prot;
+  logic [3:0] size;
   logic nonSec;
-  logic [1:0] trans;
+  logic excl;
+  logic [2:0] trans;
   logic [DataWidth - 1:0] wData;
   logic [DataWidth/8 - 1:0] wStrb;
   logic write;
-
-  logic [DataWidth - 1:0] rData;
   logic ready;
+
+  // Subordinate signals
+  logic [DataWidth - 1:0] rData;
+  logic readyOut;
   logic resp;
+  logic exOkay;
 
-  modport ctrl(
-      input clk,
-      input nReset,
+  modport manager(
+    input clk,
+    input nReset,
 
-      output addr,
-      output burst,
-      output mastLock,
-      output size,
-      output nonSec,
-      output trans,
-      output wData,
-      output wStrb,
-      output write,
+    input ready,
+    input resp,
+    
+    input rData,
 
-      input rData,
-      input ready,
-      input resp
+    output addr,
+    output write,
+    output size,
+    output burst,
+    output prot,
+    output trans,
+    output mastLock,
+
+    output wData
   );
 
-  modport prph(
-      input clk,
-      input nReset,
+  modport subordinate(
+    input clk,
+    input nReset,
 
-      input addr,
-      input burst,
-      input mastLock,
-      input size,
-      input nonSec,
-      input trans,
-      input wData,
-      input wStrb,
-      input write,
+    input addr,
+    input write,
+    input size,
+    input burst,
+    input prot,
+    input trans,
+    input mastLock,
+    input ready,
 
-      input ready
+    input wData,
+
+    output readyOut,
+    output resp,
+
+    output rData
   );
 
-  modport decoder(input clk, input nReset, input addr, input nonSec);
+  modport mux(
+    input clk,
+    input nReset,
 
-  modport mux(input clk, input nReset, output rData, output ready, output resp);
+    output resp,
+    output exOkay,
+    output ready,
+    output rData
+  );
+
+  modport decoder(
+    input clk,
+    input nReset,
+
+    input addr,
+    input nonSec
+  );
+
 endinterface
