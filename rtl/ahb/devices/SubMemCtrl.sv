1ef73294ab094c23576cf2233576974b45f87e70
rishyak <hello@rishyak.com>
2023-03-12 00:44:29 -0500

    add: mem ctrl sub
    Signed-off-by: rishyak <hello@rishyak.com>

---
+++ rtl/ahb/devices/SubMemCtrl.sv
@@ -0,0 +1,13 @@
+/**
+  @brief Subordinate to interface witht he memory controller
+
+  @note See https://github.com/NYU-Processor-Design/nyu-mem
+*/
+
+module SubMemCtrl(
+  AHBCommon_if.subordinate sub,
+  MemCommon_if.memCtrl mem
+);
+  // General subordinate logic follows
+
+endmodule
