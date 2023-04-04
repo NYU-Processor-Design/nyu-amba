1f4fd81cb977abeff2c2235aeee5263ece8b171b
rishyak <hello@rishyak.com>
2023-04-03 20:08:10 -0400

    wip(SubDummy): simple write/read
    Signed-off-by: rishyak <hello@rishyak.com>

--- dv/ahb/devices/SubDummy/SubDummy.cpp
+++ dv/ahb/devices/SubDummy/SubDummy.cpp
@@ -1,8 +1,35 @@
 #include <VSubDummy_tl.h>
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("INIT") {
+void clock(VSubDummy_tl &dut) {
+  dut.clk = 0;
+  dut.eval();
+  dut.clk = 1;
+  dut.eval();
+}
+
+TEST_CASE("Init") {
   VSubDummy_tl dut;
   dut.eval();
   REQUIRE(1 == 1);
+}
+
+TEST_CASE("Single read/write") {
+  VSubDummy_tl dut;
+  dut.control = 0;
+  dut.eval();
+  dut.addr = 0x0;
+  dut.wData = 0x1234;
+  dut.write = 1;
+  dut.trans = 2;
+  clock(dut);
+  dut.wData = 0;
+  dut.write = 0;
+  dut.trans = 2;
+  REQUIRE(dut.readyOut == 1);
+  REQUIRE(dut.resp == 0);
+  clock(dut);
+  REQUIRE(dut.readyOut == 1);
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.rData == 0x1234);
 }
--- dv/ahb/devices/SubDummy/SubDummy_tl.sv
+++ dv/ahb/devices/SubDummy/SubDummy_tl.sv
@@ -3,11 +3,30 @@ module SubDummy_tl #(
   DataWidth = 32
 ) (
   input clk,
-  input nReset
+  input nReset,
+  input [AddrWidth-1:0] addr,
+  input [DataWidth-1:0] wData,
+  input [3:0] control,
+  input [1:0] trans,
+  input write,
+
+  output [DataWidth-1:0] rData,
+  output [1:0] resp,
+  output readyOut
 );
 
   AHBCommon_if #(DataWidth, AddrWidth) ahb (clk, nReset);
 
-  SubDummy sub (.*);
+  SubDummy sub (control, .*);
+
+  assign ahb.addr = addr;
+  assign ahb.wData = wData;
+  assign ahb.trans = trans;
+  assign ahb.write = write;
+
+  assign resp = ahb.resp;
+  assign rData = ahb.rData;
+  assign readyOut = ahb.readyOut;
+
 
 endmodule
