b05bbd03c15f7f37efddc530abffcd337920f196
Vito Gamberini <vito@gamberini.email>
2023-10-23 01:49:31 -0400

    test(subdummy): Use NYUTestUtil

--- dv/ahb/SubDummy/SubDummy.cpp
+++ dv/ahb/SubDummy/SubDummy.cpp
@@ -1,38 +1,27 @@
 #include <catch2/catch_test_macros.hpp>
+#include <NyuTestUtil.hpp>
+
 #include <VSubDummy_tl.h>
 
-void clock(VSubDummy_tl& dut) {
-  dut.clk = 0;
-  dut.eval();
-  dut.clk = 1;
-  dut.eval();
-}
-
-TEST_CASE("SubDummy, Init") {
-  VSubDummy_tl dut;
-  dut.eval();
-  REQUIRE(1 == 1);
-}
-
 TEST_CASE("SubDummy, Single read write") {
-  VSubDummy_tl dut;
+  auto& dut {nyu::getDUT<VSubDummy_tl>()};
   dut.control = 0;
   dut.sel = 1;
   dut.nReset = 0;
   dut.trans = 0;
-  clock(dut);
+  nyu::tick(dut);
   dut.nReset = 1;
   dut.addr = 0x0;
   dut.wData = 0x1234;
   dut.write = 1;
   dut.trans = 2;
-  clock(dut);
+  nyu::tick(dut);
   dut.wData = 0;
   dut.write = 0;
   dut.trans = 2;
   REQUIRE(dut.readyOut == 1);
   REQUIRE(dut.resp == 0);
-  clock(dut);
+  nyu::tick(dut);
   REQUIRE(dut.readyOut == 1);
   REQUIRE(dut.resp == 0);
   REQUIRE(dut.rData == 0x1234);
