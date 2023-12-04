eaf5842ac933061fda5022976ae94c2fa54349cc
Kevin Y <kcy4870@nyu.edu>
2023-12-03 19:45:26 -0500

    improved subDummy tests

--- dv/ahb/SubDummy/SubDummy.cpp
+++ dv/ahb/SubDummy/SubDummy.cpp
@@ -26,3 +26,122 @@ TEST_CASE("SubDummy, Single read write") {
   REQUIRE(dut.resp == 0);
   REQUIRE(dut.rData == 0x1234);
 }
+
+TEST_CASE("SubDummy Unslected") {
+  VSubDummy_tl dut;
+  dut.control = 0;
+  dut.sel = 0;
+  dut.nReset = 0;
+  dut.trans = 0;
+  clock(dut);
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.readyOut == 1);
+}
+
+TEST_CASE("SubDummy Sequential Transfer with busy cycles") {
+  VSubDummy_tl dut;
+  //Reset
+  dut.control = 0;
+  dut.sel = 1;
+  dut.nReset = 0;
+  dut.trans = 0;
+  clock(dut);
+  //Write address 0x0
+  dut.nReset = 1;
+  dut.addr = 0x0;
+  dut.wData = 0x1234;
+  dut.write = 1;
+  dut.trans = 2;
+  clock(dut);
+  //Write address 0x1
+  REQUIRE(dut.resp == 0);
+  dut.addr = 0x1;
+  dut.wData = 0x5678;
+  dut.trans = 3;
+  clock(dut);
+  //Write address 0x2 with busy cycle
+  REQUIRE(dut.resp == 0);
+  dut.addr = 0x2;
+  dut.wData = 0x9abc;
+  dut.trans = 1;
+  clock(dut);
+  //Manager continues but subordinate stalls
+  REQUIRE(dut.resp == 0);
+  dut.trans = 3;
+  dut.control = 1;
+  clock(dut);
+  //Manager holds values as subordinate reasserts readyOut
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.readyOut == 0);
+  dut.control = 0;
+  clock(dut);
+  //Complete transferns read address 0x0
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.readyOut == 1);
+
+  dut.write = 0;
+  dut.addr = 0x0;
+  dut.trans = 2;
+  dut.control = 0;
+  clock(dut);
+  //Complete transferns read address 0x1
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.readyOut == 1);
+  REQUIRE(dut.rData == 0x1234);
+  dut.trans = 3;
+  dut.addr = 0x1;
+  clock(dut);
+  //Complete transferns read address 0x2
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.readyOut == 1);
+  REQUIRE(dut.rData == 0x5678);
+  dut.addr = 0x2;
+  clock(dut);
+  //Complete transferns read address 0x3
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.readyOut == 1);
+  REQUIRE(dut.rData == 0x9abc);
+}
+
+TEST_CASE("SubDummy Idle state") {
+  VSubDummy_tl dut;
+  //Reset
+  dut.control = 0;
+  dut.sel = 1;
+  dut.nReset = 0;
+  dut.trans = 0;
+  clock(dut);
+  dut.nReset = 1;
+  clock(dut);
+  clock(dut);
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.readyOut == 1);
+}
+
+TEST_CASE("SubDummy Error Resolution") {
+  VSubDummy_tl dut;
+  //Reset
+  dut.control = 0;
+  dut.sel = 1;
+  dut.nReset = 0;
+  dut.trans = 0;
+  clock(dut);
+  //Set SubDummy to trigger an error
+  dut.nReset = 1;
+  dut.trans = 2;
+  dut.write = 1;
+  dut.wData = 0x1234;
+  dut.addr = 0x0;
+  dut.control = 2;
+  clock(dut);
+  //SubDummy should respond with an error and not process anything. 
+  //Set transfer to idle to resolve error
+  REQUIRE(dut.resp == 1);
+  REQUIRE(dut.readyOut == 0);
+  dut.control = 0;
+  dut.trans = 0;
+  clock(dut);
+  //SubDummy should be ready to process again
+  REQUIRE(dut.resp == 0);
+  REQUIRE(dut.readyOut == 1);
+}
--- rtl/ahb/devices/SubDummy.sv
+++ rtl/ahb/devices/SubDummy.sv
@@ -33,58 +33,71 @@ module SubDummy (
   } Control;  // Dummy control signal for testing
 
 
-  always_ff @(posedge ahb.clk or negedge ahb.nReset) begin
-    if (~ahb.nReset) begin
-      ahb.readyOut <= 1'b1;
-    end else begin
-      //If subordinate is not selected, respond with okay and idle
-      if (~ahb.sel) begin
-        ahb.resp <= AHBCommon_pkg::RESP_OKAY;
-        ahb.readyOut <= 1'b1;
-        state <= AHBCommon_pkg::STATE_IDLE;
-      end else begin
-        case (ahb.trans)
-          //Manager is idle or busy
-          AHBCommon_pkg::TRANS_IDLE: begin
-            case (state)
-              //Wait until transfer continues or ends
-              AHBCommon_pkg::STATE_IDLE, AHBCommon_pkg::STATE_READ, AHBCommon_pkg::STATE_WRITE: begin
-                ahb.readyOut <= 1'b1;
-              end
-              AHBCommon_pkg::STATE_ERROR: begin
-                //Only set readyOut to 1 if the manager returns to Idle
-                ahb.readyOut <= 1'b0;
-                state <= AHBCommon_pkg::STATE_IDLE;
-              end
-            endcase
-          end
-          AHBCommon_pkg::TRANS_BUSY: begin
-            //Doesn't do anything yet
+    always_ff @(posedge ahb.clk or negedge ahb.nReset) begin 
+        if (~ahb.nReset) begin
             ahb.readyOut <= 1'b1;
-          end
-          //Manager is starting a new transfer
-          AHBCommon_pkg::TRANS_NONSEQ: begin
-            case (state)
-              AHBCommon_pkg::STATE_IDLE, AHBCommon_pkg::STATE_READ, AHBCommon_pkg::STATE_WRITE: begin
-                //Perform read and writes to modules
-                if (ahb.write && control == CONTROL_OK) begin
-                  mem[ahb.addr] <= ahb.wData;
-                  state <= AHBCommon_pkg::STATE_WRITE;
-                end else begin
-                  ahb.rData <= mem[ahb.addr];
-                  state <= AHBCommon_pkg::STATE_READ;
-                end
-              end
-              AHBCommon_pkg::STATE_ERROR: begin
-                state <= AHBCommon_pkg::STATE_ERROR;
-              end
-            endcase
-          end
-          AHBCommon_pkg::TRANS_SEQ: begin
-            //Doesn't do anything yet
-            state <= AHBCommon_pkg::STATE_IDLE;
-          end
-        endcase
+        end else begin
+            //If subordinate is not selected, respond with okay and idle
+            if (~ahb.sel) begin
+                ahb.resp <= AHBCommon_pkg::RESP_OKAY;
+                ahb.readyOut <= 1'b1;
+                state <= AHBCommon_pkg::STATE_IDLE;
+            end else begin
+                case (ahb.trans)
+                    //Manager is idle or busy
+                    AHBCommon_pkg::TRANS_IDLE: begin
+                        case (state)
+                            //Wait until transfer continues or ends
+                            AHBCommon_pkg::STATE_IDLE, AHBCommon_pkg::STATE_READ, AHBCommon_pkg::STATE_WRITE: begin
+                                ahb.readyOut <= 1'b1;
+                            end
+                            AHBCommon_pkg::STATE_ERROR: begin
+                                //Only set readyOut to 1 if the manager returns to Idle
+                                ahb.readyOut <= 1'b0;
+                                state <= AHBCommon_pkg::STATE_IDLE;
+                            end
+                        endcase
+                    end
+                    AHBCommon_pkg::TRANS_BUSY: begin
+                        //Doesn't do anything yet
+                        ahb.readyOut <= 1'b1;
+                    end
+                    //Manager is starting a new transfer
+                    AHBCommon_pkg::TRANS_NONSEQ: begin
+                        case (state)
+                            AHBCommon_pkg::STATE_IDLE, AHBCommon_pkg::STATE_READ, AHBCommon_pkg::STATE_WRITE: begin
+                                //Perform read and writes to modules
+                                if (ahb.write && control == CONTROL_OK) begin
+                                    mem[ahb.addr] <= ahb.wData;
+                                    state <= AHBCommon_pkg::STATE_WRITE;
+                                end else begin
+                                    ahb.rData <= mem[ahb.addr];
+                                    state <= AHBCommon_pkg::STATE_READ;
+                                end
+                            end
+                            AHBCommon_pkg::STATE_ERROR: begin
+                                state <= AHBCommon_pkg::STATE_ERROR;
+                            end
+                        endcase
+                    end
+                    AHBCommon_pkg::TRANS_SEQ: begin
+                        case (state)
+                            AHBCommon_pkg::STATE_READ, AHBCommon_pkg::STATE_WRITE: begin
+                                //Perform read and writes to modules
+                                if (ahb.write && control == CONTROL_OK) begin
+                                    mem[ahb.addr] <= ahb.wData;
+                                    state <= AHBCommon_pkg::STATE_WRITE;
+                                end else begin
+                                    ahb.rData <= mem[ahb.addr];
+                                    state <= AHBCommon_pkg::STATE_READ;
+                                end
+                            end
+                            default: begin
+                                state <= AHBCommon_pkg::STATE_ERROR;
+                            end
+                        endcase
+                    end
+                endcase
 
         //Set reponse based on control signals
         case (control)
